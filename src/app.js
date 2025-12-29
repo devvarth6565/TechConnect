dad399665194a6be1a2a32f5baae9ca4a2043ec5
devvarth Singh <devvarthsinghwork@gmail.com>
2025-12-29 19:10:29 +0530

    cors and connecting with fronten

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ const requestRouter = require("./router/request.js");
 const profileRouter = require("./router/profile.js");
 const authRouter = require("./router/auth.js");
 const userRouter = require("./router/user.js");
+const cors = require("cors");
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser())
 
